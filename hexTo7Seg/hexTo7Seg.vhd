e3b1edc87194c3820cf013c52060efbc6536490d
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-09-11 20:57:24 -0300

    fix: change hexto7seg to common anode

--- hexTo7Seg/hexTo7Seg.vhd
+++ hexTo7Seg/hexTo7Seg.vhd
@@ -17,15 +17,15 @@ begin
 
 	data_in_s <= DataIn;
 
-	DataOut <= "00000110" when data_in_s = "0001"
-					else "01011011" when data_in_s = "0010"
-					else "01001111" when data_in_s = "0011"
-					else "01100110" when data_in_s = "0100"
-					else "01101101" when data_in_s = "0101"
-					else "01111101" when data_in_s = "0110"
-					else "00000111" when data_in_s = "0111"
-					else "01111111" when data_in_s = "1000"
-					else "01101111" when data_in_s = "1001"
-					else "00111111";
+	DataOut <= "11111001" when data_in_s = "0001"
+					else "10100100" when data_in_s = "0010"
+					else "10110000" when data_in_s = "0011"
+					else "10011001" when data_in_s = "0100"
+					else "10010010" when data_in_s = "0101"
+					else "10000010" when data_in_s = "0110"
+					else "11111000" when data_in_s = "0111"
+					else "10000000" when data_in_s = "1000"
+					else "10010000" when data_in_s = "1001"
+					else "11000000";
 					
 end architecture;
