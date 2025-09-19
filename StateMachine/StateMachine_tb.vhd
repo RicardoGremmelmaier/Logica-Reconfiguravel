6ca9ba0270e8e35d4a7d27cf622075e230f6b567
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-09-18 22:43:02 -0300

    fix: fix state machine tb

--- StateMachine/StateMachine_tb.vhd
+++ StateMachine/StateMachine_tb.vhd
@@ -8,59 +8,81 @@ end;
 architecture func of StateMachine_tb is
     component StateMachine is 
     port (
-        Clr_btn, Pause_btn, RST: in std_logic;
-        State: out std_logic_vector(1 downto 0)
+        clk      : in std_logic;                        -- Clock
+        RST      : in std_logic;                        -- Reset assíncrono
+        Clr_btn  : in std_logic;                        -- Botão CLR
+        Pause_btn: in std_logic;                        -- Botão PAUSE/RESUME
+        State    : out std_logic_vector(1 downto 0)     -- Estado atual
     );
 end component StateMachine;
 
-signal Clr_btn_s, Pause_btn_s, RST : std_logic := '1';
+signal Clr_btn_s, Pause_btn_s, RST, clk : std_logic := '1';
 signal State_s: std_logic_vector(1 downto 0) := "00";
 constant period_time : time := 100 ns;
+signal finished : std_logic := '0';
 
 begin 
     UUT: StateMachine port map(
+        clk => clk,
         Clr_btn => Clr_btn_s,
         Pause_btn => Pause_btn_s,
         RST => RST,
         State => State_s
     );
-        reset_global: process
-        begin   
-            RST <= '1';
-            wait for period_time*2;
-            RST <= '0';
-            wait;
-        end process;
 
-        process
-        begin
-            wait for 400 ns;
-            wait for 150 ns;
-            Clr_btn_s <= '0';
-            wait for 150 ns;
-            Clr_btn_s <= '1';
-            wait for 300 ns;
-            Pause_btn_s <= '0';
-            wait for 150 ns;
-            Pause_btn_s <= '1';
-            wait for 300 ns;
-            Pause_btn_s <= '0';
-            wait for 150 ns;
-            Pause_btn_s <= '1';
-            wait for 300 ns;
-            Pause_btn_s <= '0';
-            wait for 150 ns;
-            Pause_btn_s <= '1';
-            wait for 300 ns;
-				Pause_btn_s <= '0';
-				wait for 150 ns;
-				Pause_btn_s <= '1';
-				wait for 300 ns;
-            Clr_btn_s <= '0';
-            wait for 150 ns;
-            Clr_btn_s <= '1';
-            wait for 500 ns;
-    
+    -- Clock
+	clk_proc: process
+	begin
+		while finished /= '1' loop
+			clk <= '0';
+			wait for period_time/2;
+			clk <= '1';
+			wait for period_time/2;
+		end loop;
+		wait;
+	end process clk_proc;
+
+    reset_global: process
+    begin   
+        RST <= '1';
+        wait for period_time*2;
+        RST <= '0';
+        wait;
+    end process;
+
+    process
+    begin
+        wait for 400 ns;
+        wait for 150 ns;
+        Clr_btn_s <= '0';
+        wait for 150 ns;
+        Clr_btn_s <= '1';
+        wait for 300 ns;
+        Pause_btn_s <= '0';
+        wait for 150 ns;
+        Pause_btn_s <= '1';
+        wait for 600 ns;
+        Clr_btn_s <= '0';
+        wait for 150 ns;
+        Clr_btn_s <= '1';
+        wait for 300 ns;
+        Pause_btn_s <= '0';
+        wait for 150 ns;
+        Pause_btn_s <= '1';
+        wait for 300 ns;
+        Pause_btn_s <= '0';
+        wait for 150 ns;
+        Pause_btn_s <= '1';
+        wait for 300 ns;
+        Pause_btn_s <= '0';
+        wait for 150 ns;
+        Pause_btn_s <= '1';
+        wait for 300 ns;
+        Clr_btn_s <= '0';
+        wait for 150 ns;
+        Clr_btn_s <= '1';
+        wait for 500 ns;
+
         wait;
     end process;
 end func;
